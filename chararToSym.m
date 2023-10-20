9b4a71429287d338ce4e19fabc889387498bf76e
Pietro Cimarosto <96522352+pietrocima@users.noreply.github.com>
2023-10-20 10:25:10 +0200

    f from int to string

--- chararToSym.m
+++ chararToSym.m
@@ -1,8 +1,10 @@
 function longSym = chararToSym(number)
     mustBeA(number, 'char');
     longSym = sym(0);
+
     for l = 1:length(number)
-        digit = str2num(number(l));
+        digit = str2double(number(l));
         longSym = longSym + (sym(digit) * sym(10^(length(number)-l)));
     end
+
 end
